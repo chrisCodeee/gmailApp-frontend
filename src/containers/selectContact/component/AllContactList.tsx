18a324d5d6cf1d02802ae78eb7216a358295b2b4
Chris <nzeanoruec@gmail.com>
2024-07-31 14:10:42 -0400

    Added responsiveness

--- src/containers/selectContact/component/AllContactList.tsx
+++ src/containers/selectContact/component/AllContactList.tsx
@@ -1,18 +1,14 @@
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { AllContactListWrapper } from "../SelectContactStyle";
-import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
-import { useCompose } from "../../../hooks";
-import { useEffect, useState } from "react";
-import { useComposeMessageState } from "../../../state-management";
 
 export const contactListItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
 
 const AllContactList = () => {
-	const { useComposeMessage } = useCompose();
+	// const { useComposeMessage } = useCompose();
 
 	// const [isActive, setIsActive] = useState(false);
 
-	const [index, setIndex] = useState<string>();
+	// const [index, setIndex] = useState<string>();
 
 	return (
 		<>
