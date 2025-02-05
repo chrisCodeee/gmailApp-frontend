38a148c427f5bfe22680fabb43c555e37073a599
Chris <nzeanoruec@gmail.com>
2025-02-05 10:53:27 -0500

    removed the user auth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import * as Container from "../containers";
 import { AppWrapper } from "./AppStyles";
 import { useAsideState, useInboxState, useNavBarState } from "../state-management";
 import { ComposeMessageMinimized } from "../containers/compose/component";
-import { useCompose, useUser } from "../hooks";
+import { useCompose } from "../hooks";
 import { ScheduleCheckEmailPopUp, ScheduleCheckPopUp } from "../containers/scheduleSend/component";
 import MoreLabelOption from "../containers/moreOptions/subMoreOptions/MoreLabelOption";
 import MoreOptionTimeAndDate from "../containers/moreOptionSetUpTimeAndDate/MoreOptionTimeAndDate";
@@ -22,7 +22,7 @@ function App() {
 
 	const { selectMessageTypeState, markAllMessageReadState, selectInputToolState, messageSentState, setMessage } = useInboxState();
 
-	const { user } = useUser();
+	// const { user } = useUser();
 	// const { user, username } = useUser();
 
 	useEffect(() => {
@@ -46,62 +46,62 @@ function App() {
 
 	return (
 		<>
-			{user.userName ? (
-				<>
-					<AppWrapper>
-						<div className="d-none d-xl-block">
-							<Container.NavBar />
-						</div>
+			{/* {user.userName ? ( */}
+			<>
+				<AppWrapper>
+					<div className="d-none d-xl-block">
+						<Container.NavBar />
+					</div>
 
-						<div className="d-block d-xl-none animate__animated animate__slideInRight">
-							<>
-								<GetGmailApp />
-								<Container.MobileNavBar />
-							</>
-						</div>
+					<div className="d-block d-xl-none animate__animated animate__slideInRight">
+						<>
+							<GetGmailApp />
+							<Container.MobileNavBar />
+						</>
+					</div>
 
-						{/* {mobileMenuState && <Container.MobileViewMenu />} */}
-						{mobileComposeState && <Container.MobileCompose />}
-						{mobileSuccessMessageState && <SuccessMessage />}
-						{/* <SuccessMessage /> */}
+					{/* {mobileMenuState && <Container.MobileViewMenu />} */}
+					{mobileComposeState && <Container.MobileCompose />}
+					{mobileSuccessMessageState && <SuccessMessage />}
+					{/* <SuccessMessage /> */}
 
-						<div className="d-block d-xl-none animate__animated animate__slideInRight">
-							<Outlet />
-						</div>
+					<div className="d-block d-xl-none animate__animated animate__slideInRight">
+						<Outlet />
+					</div>
 
-						<div className="d-none d-xl-block">
-							<Outlet />
-						</div>
+					<div className="d-none d-xl-block">
+						<Outlet />
+					</div>
 
-						{/* Other Components when clicked or hovered */}
-						{createLabelState && <Container.CreateLabelModal />}
-						{googleAppState && <Container.GoogleApps />}
-						{showMoreState && <Container.SearchMoreOptions />}
-						{supportState && <Container.Support />}
-						{accountProfileState && <Container.AccountProfile />}
-						{selectMessageTypeState && <Container.SelectMessageType />}
-						{markAllMessageReadState && <Container.MarkAllAsRead />}
-						{selectInputToolState && <Container.LanguageInputTool />}
-						{composeMessageState && <Container.Compose />}
-						{useComposeMessage.composeMessageMinimizeState && <ComposeMessageMinimized />}
-						{useComposeMessage.insertLinkState && <Container.InsertLink />}
-						{useComposeMessage.alertState && <Container.Alert alertName="The URL is not valid and cannot be loaded." />}
-						{useComposeMessage.notActiveState && <Container.Alert alertName="Sorry, this is yet to be implemented. Thank you for checking out my work." />}
-						{useComposeMessage.confidentialModeState && <Container.ConfidentialMode />}
-						{useComposeMessage.insertSignatureState && <Container.InsertSignature />}
-						{useComposeMessage.scheduleSendPopUpState && <Container.ScheduleSendPopUp />}
-						{useComposeMessage.scheduleCheckEmailPopUpState && <ScheduleCheckEmailPopUp />}
-						{useComposeMessage.moreLabelOptionState && <MoreLabelOption />}
-						{useComposeMessage.moreOptionTimeAndDateState && <MoreOptionTimeAndDate />}
-						{useComposeMessage.scheduleAppointmentState && <ScheduleAppointment />}
-						{useComposeMessage.scheduleSendCheckPopUpState && <ScheduleCheckPopUp />}
-						{messageSentState && <Container.MessageSentAlert />}
-						{/* <Container.SelectContact /> */}
-					</AppWrapper>
-				</>
-			) : (
+					{/* Other Components when clicked or hovered */}
+					{createLabelState && <Container.CreateLabelModal />}
+					{googleAppState && <Container.GoogleApps />}
+					{showMoreState && <Container.SearchMoreOptions />}
+					{supportState && <Container.Support />}
+					{accountProfileState && <Container.AccountProfile />}
+					{selectMessageTypeState && <Container.SelectMessageType />}
+					{markAllMessageReadState && <Container.MarkAllAsRead />}
+					{selectInputToolState && <Container.LanguageInputTool />}
+					{composeMessageState && <Container.Compose />}
+					{useComposeMessage.composeMessageMinimizeState && <ComposeMessageMinimized />}
+					{useComposeMessage.insertLinkState && <Container.InsertLink />}
+					{useComposeMessage.alertState && <Container.Alert alertName="The URL is not valid and cannot be loaded." />}
+					{useComposeMessage.notActiveState && <Container.Alert alertName="Sorry, this is yet to be implemented. Thank you for checking out my work." />}
+					{useComposeMessage.confidentialModeState && <Container.ConfidentialMode />}
+					{useComposeMessage.insertSignatureState && <Container.InsertSignature />}
+					{useComposeMessage.scheduleSendPopUpState && <Container.ScheduleSendPopUp />}
+					{useComposeMessage.scheduleCheckEmailPopUpState && <ScheduleCheckEmailPopUp />}
+					{useComposeMessage.moreLabelOptionState && <MoreLabelOption />}
+					{useComposeMessage.moreOptionTimeAndDateState && <MoreOptionTimeAndDate />}
+					{useComposeMessage.scheduleAppointmentState && <ScheduleAppointment />}
+					{useComposeMessage.scheduleSendCheckPopUpState && <ScheduleCheckPopUp />}
+					{messageSentState && <Container.MessageSentAlert />}
+					{/* <Container.SelectContact /> */}
+				</AppWrapper>
+			</>
+			{/* ) : (
 				<Navigate to="/login" />
-			)}
+			)} */}
 		</>
 	);
 }
